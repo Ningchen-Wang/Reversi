509659a99b0653aa7cbb8ed5b5cc363aba973ea8
SunshineAllWay <mhnfwln@126.com>
2015-03-19 14:37:07 +0800

    Add test1

--- Reversi/main.asm
+++ Reversi/main.asm
@@ -17,6 +17,20 @@ start:
 	exit
 
 ;----------------------------------------------------
+JudgeInGrid PROC,
+	x:DWORD, y:DWORD
+;judge if (x, y) is in the grid
+;if yes, eax = 1;
+;else, eax = 0
+	.IF (x < 0 || x > 7 || y < 0 || y > 7)
+		mov eax, 0
+	.ELSE
+		mov eax, 1
+	.ENDIF
+	ret
+JudgeInGrid ENDP
+
+;----------------------------------------------------
 GetMapAddress PROC, 
 	x:DWORD, y:DWORD, pmap:PTR DWORD
 ;calculate the address of (x, y) in map
@@ -61,6 +75,107 @@ GetXYAddress PROC,
 		ret
 	.ENDIF
 GetXYAddress ENDP
+
+;--------------------------------------------------------------------------------------------
+;this block is used to check whether the step is valid, whose turn is equal to var turn
+;we will check in eight directions, which respond to eight checking functions
+;1-direction vector(1, 0)  2-direction vector(-1, 0)
+;3-direction vector(0, 1)  4-direction vector(0, -1)
+;5-direction vector(1, 1)  6-direction vector(-1, 1)
+;7-direction vector(1, -1)  8-direction vector(-1, -1)
+
+Test1 PROC USES esi edi edx,
+	x:DWORD, y:DWORD, pmap:PTR DWORD, pturn:DWORD
+;by the direction vector (1, 0)
+;current turn is pturn
+;if it is valid step, ebx = 1;else ebx = 0
+	local opposite:DWORD
+	local xystate:DWORD
+
+	mov ebx, 3
+	sub ebx, pturn
+	mov opposite, ebx
+
+	INVOKE JudgeInGrid, x, y
+	.IF (eax == 0)
+		mov ebx, 0
+		ret
+	.ENDIF
+	 
+	INVOKE GetMapAddress, x, y, pmap
+	mov esi, pmap
+	add esi, eax
+	push ebx
+	mov ebx, [esi]
+	mov xystate, ebx
+	pop ebx
+	
+	.IF (xystate != 0 || x == 7 || x == 6)
+		mov ebx, 0
+		ret
+	.ENDIF
+
+	mov esi, x
+	add esi, 1
+	mov edi, y
+	push esi
+	push edi
+	INVOKE GetMapAddress, esi, edi, pmap
+	mov esi, pmap
+	add esi, eax
+	push ebx
+	mov ebx, [esi]
+	mov xystate, ebx
+	pop ebx
+	pop edi
+	pop esi
+
+	.IF (xystate == turn || xystate == 0)
+		mov ebx, 0
+		ret
+	.ENDIF
+
+	mov esi, x
+	mov edi, y
+	mov eax, 1
+	mov edx, 1
+	add esi, 1
+	.WHILE (eax == 1 && edx == 1)
+		push esi
+		push edi
+		INVOKE GetMapAddress, esi, edi, pmap
+		mov esi, pmap
+		add esi, eax
+		push ebx
+		mov ebx, [esi]
+		mov xystate, ebx
+		pop ebx
+		pop edi
+		pop esi
+		
+		.IF (xystate == opposite)
+			mov edx, 1
+		.ELSEIF
+			mov edx, 0
+		.ENDIF
+
+		add esi, 1
+		INVOKE JudgeInGrid, esi, edi
+	.ENDW
+
+	.IF (edx == 0 && xystate == pturn)
+		mov ebx, 1
+	.ELSE
+		mov ebx, 0
+	.ENDIF
+	ret
+TEST1 ENDP
+
+
+	
+	
+
+;--------------------------------------------------------------------------------------------
 
 ;----------------------------------------------------
 InitMap PROC, pturn:PTR DWORD, pmap:PTR DWORD, pblack_count:PTR DWORD, pwhite_count:PTR DWORD
@@ -132,6 +247,8 @@ main PROC
 	
 	mov ad, eax
 	INVOKE GetXYAddress, ad, addr map
+
+	INVOKE Test1, 2, 4, addr map, 1 
 
 	ret
 main ENDP
