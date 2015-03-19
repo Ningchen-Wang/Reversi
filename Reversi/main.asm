76f6159de418903583fc60e3350620a8bacdcf06
SunshineAllWay <mhnfwln@126.com>
2015-03-19 12:39:34 +0800

    Add coordinate and address transfer function

--- Reversi/main.asm
+++ Reversi/main.asm
@@ -17,10 +17,55 @@ start:
 	exit
 
 ;----------------------------------------------------
+GetMapAddress PROC, 
+	x:DWORD, y:DWORD, pmap:PTR DWORD
+;calculate the address of (x, y) in map
+;the beginning address of map is pmap 
+;return the result in eax
+	push esi
+	.IF (x < 0 || x > 7 || y < 0 || y > 7)
+		mov eax, pmap
+	.ELSE
+		mov eax, 8
+		mul x
+		add eax, y
+		mov esi, 4
+		mul esi
+		add eax, pmap
+	.ENDIF
+	pop esi
+	ret
+GetMapAddress ENDP
+
+;----------------------------------------------------
+GetXYAddress PROC,
+	mapAddress:DWORD, pmap:PTR DWORD
+;calculate the (x ,y) address of the point whose map address is mapAddress
+;store x in eax, and y in edx
+	mov eax, mapAddress
+	sub eax, pmap
+	.IF (eax < 0 || eax > 252)
+		mov eax, 0
+		mov edx, 0
+		ret
+	.ELSE
+		mov edx, 0
+		push esi 
+		mov esi, 4
+		div esi
+		
+		mov esi, 8
+		mov edx, 0
+		div esi
+		pop esi
+		ret
+	.ENDIF
+GetXYAddress ENDP
+
+;----------------------------------------------------
 InitMap PROC, pturn:PTR DWORD, pmap:PTR DWORD, pblack_count:PTR DWORD, pwhite_count:PTR DWORD
 ;initilize the var in main
-	push eax
-	push ebx
+	pushad
 
 	mov eax, pturn
 	mov ebx, 1
@@ -44,10 +89,31 @@ InitMap PROC, pturn:PTR DWORD, pmap:PTR DWORD, pblack_count:PTR DWORD, pwhite_co
 		add esi, 4
 		loop L1
 
-	mov [pmap + 108], 1
-	mov [pmap + 144], 1
-	mov [pmap + 112], 2
-	mov [pmap + 140], 2
+	mov eax, pmap
+	mov esi, 108
+	add eax, esi
+	mov ebx, 1
+	mov [eax], ebx
+
+	mov eax, pmap
+	mov esi, 144
+	add eax, esi
+	mov ebx, 1
+	mov [eax], ebx
+
+	mov eax, pmap
+	mov esi, 112
+	add eax, esi
+	mov ebx, 2
+	mov [eax], ebx
+
+	mov eax, pmap
+	mov esi, 140
+	add eax, esi
+	mov ebx, 2
+	mov [eax], ebx
+
+	popad
 
 	;call GUI function
 
@@ -59,8 +125,13 @@ main PROC
 	local map[64]:DWORD
 	local black_count:DWORD
 	local white_count:DWORD
+	local ad:DWORD
 
 	INVOKE InitMap, addr turn, addr map, addr black_count, addr white_count
+	INVOKE GetMapAddress, 4, 4, addr map
+	
+	mov ad, eax
+	INVOKE GetXYAddress, ad, addr map
 
 	ret
 main ENDP
