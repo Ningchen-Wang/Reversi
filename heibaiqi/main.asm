3b29b83d142611a2ff1d006e0903007e480c20c2
SunshineAllWay <mhnfwln@126.com>
2015-03-19 10:34:46 +0800

    local var

--- heibaiqi/main.asm
+++ heibaiqi/main.asm
@@ -1,31 +1,60 @@
 ;heibaiqi_main
-;Author: wangningchen
+;Author: wangningchen, wangchengpeng
 ;Create: 2015/3/18
 ;Last modify: 2015/3/18
 ;Main logic entry
 
 ;--------------------------------------------------------------------------
+include \masm32\include\masm32rt.inc
 
-      include \masm32\include\masm32rt.inc
-
-        .data
+.data
 
 
 ;--------------------------------------------------------------------------
+.code
+start:
+	call main
+	exit
 
-        .code
+;----------------------------------------------------
+InitMap PROC, pturn:PTR DWORD, pmap:PTR DWORD, pblack_count:PTR DWORD, pwhite_count:PTR DWORD
+;initilize the var in main
+	;mov eax, [ebp+8]
+	;mov ebx, 1
+	;mov [eax], ebx
+	mov eax, pturn
+	mov ebx, 1
+	mov [eax], ebx
+	mov [pblack_count], 2
+	mov [pwhite_count], 2
 
-main proc
-		local turn:DWORD 
-		local map:DWORD 64 DUP(?)
-		local black_count:DWORD
-		local white_count:DWORD
-init:
-		invoke init, addr turn, addr map, addr black_count, addr white_count
-		mov retval, eax
-		cmp eax, 0
-		jne fin
-fin:
-		exit
-main endp
-end main
+	mov ecx, 64
+	mov esi, 0
+	L1:
+		mov [pmap + esi], 0
+		add esi, 4
+		loop L1
+
+	mov [pmap + 108], 1
+	mov [pmap + 144], 1
+	mov [pmap + 112], 2
+	mov [pmap + 140], 2
+
+	;call GUI function
+
+	ret
+InitMap ENDP
+
+main PROC
+	local turn:DWORD 
+	local map[64]:DWORD
+	local black_count:DWORD
+	local white_count:DWORD
+
+	INVOKE InitMap, addr turn, addr map, addr black_count, addr white_count
+
+	ret
+main ENDP
+END main
+
+END start
